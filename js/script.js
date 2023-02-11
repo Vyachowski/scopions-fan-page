76408e757b34a0322da36c89f92aaacdfac1e1e6
Vyacheslav Andreev <97975418+Vyachowski@users.noreply.github.com>
2023-02-11 23:42:28 +0200

    Fix path to a JS script

--- js/script.js
+++ js/script.js
@@ -7,15 +7,3 @@ navMain.classList.remove('main-nav--nojs');
 navToggle.addEventListener('click', function () {
   navMenu.classList.toggle('site-menu--opened');
 });
-
-/*
-navToggle.addEventListener('click', function () {
-  if (navToggle.classList.contains('main-nav--mobile-opened')) {
-    navToggle.classList.remove('main-nav--closed');
-    navToggle.classList.add('main-nav--mobile-opened');
-  } else {
-    navToggle.classList.add('main-nav--closed');
-    navToggle.classList.remove('main-nav--opened');
-  }
-});
-*/
